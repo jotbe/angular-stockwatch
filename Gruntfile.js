2028c581181900211e2b3b0a7650e68e9abbedef
Jan Beilicke <dev@jotbe-fx.de>
2013-09-16 22:15:29 +0200

    Changed LiveReload port which conflicted with Sublime Text's LiveReload plugin.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,6 @@
 // Generated on 2013-08-18 using generator-angular 0.3.1
 'use strict';
-var LIVERELOAD_PORT = 35729;
+var LIVERELOAD_PORT = 35728;
 var lrSnippet = require('connect-livereload')({ port: LIVERELOAD_PORT });
 var mountFolder = function (connect, dir) {
   return connect.static(require('path').resolve(dir));
